c2efaf83224e3f5845f93a851d3e0b24815efb3a
Tim Twiest <itsvanture@gmail.com>
2020-05-22 17:06:02 +0200

    Cleaned up 'create-react-app' to just have the stuff we need

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>
+        <span role="img" aria-label="icon">
+          🕵️‍♀️
+        </span>{' '}
+        Pathfinding-Visualizer
+      </h1>
+    </div>
+  );
+};
+
+export default App;
